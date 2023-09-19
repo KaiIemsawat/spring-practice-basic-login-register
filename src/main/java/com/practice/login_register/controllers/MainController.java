c8c53b4d758e2ff88a4cc822e146c169bd3876b9
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-09-18 21:15:36 -0400

    setup login-register module

---
+++ src/main/java/com/practice/login_register/controllers/MainController.java
@@ -0,0 +1,36 @@
+package com.practice.login_register.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String r_loginPage() {
+        return null;
+    }
+
+    @GetMapping("/dashboard")
+    public String r_dashboard() {
+        return null;
+    }
+
+    @PostMapping("/register")
+    public String p_registerUser() {
+        return null;
+    }
+
+    @PostMapping("/login")
+    public String p_login() {
+        return null;
+    }
+
+//    Log out by clearing session and sending back to login/register page
+    @GetMapping("/logout")
+    public String p_logout() {
+        return null;
+    }
+
+}
